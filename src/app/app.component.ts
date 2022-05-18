becc1118611f3ac0a1a13b19cd8b6db9df7788c0
claudiucirtina <98809142+claudiucirtina@users.noreply.github.com>
2022-05-18 14:28:46 +0200

    cocktails list et details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {interval, Observable, Observer, Subject} from "rxjs";
-import {Cocktail} from "./interfaces/cocktail.interface";
 
 @Component({
   selector: 'app-root',
